3eb1c30c46c3eaff466b35e24e9d941dbaf65e47
David <ekechukwudavid@gmail.com>
2024-02-22 22:26:12 +0530

    change of python version

--- main/urls.py
+++ main/urls.py
@@ -1,10 +1,5 @@
 from django.urls import path
 from . import views
-from django.http import HttpResponseNotFound
-from django.shortcuts import render, Http404
-
-def custom_404_view(request, exception):
-    return render(request, '404.html', status=404)
 
 urlpatterns = [
     path('', views.home, name='home'),
@@ -14,4 +9,3 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('gallery/', views.gallery, name='gallery'),
 ]
-handler404 = custom_404_view
